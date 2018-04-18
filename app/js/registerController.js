48175e5abcf8d8777e5594ea189ec155665424b2
AudreyDan <251918489@qq.com>
2018-04-18 20:51:20 +0800

    修改注册页面和样式

--- app/js/registerController.js
+++ app/js/registerController.js
@@ -3,19 +3,35 @@
     var app = angular.module('resumeApp',[]);
     app.controller("registerController", ["$scope", "registerService","$rootScope", function ($scope, registerService, $rootScope) {
         $scope.register = function(user){
-            if (user.nickname != "" && user.account != null && user.password != null){
-
-                registerService.userRegister(user,function(data){
-                    console.log(data)
+            if (user.account != "" && user.password != null && user.surePwd != null){
+                if (user.password == user.surePwd){
+                    var data = {
+                        "account" : user.account,
+                        "password" : user.password
+                    };
+                    registerService.userRegister(data,function(data){
+                        console.log(data)
+                        swal({
+                            title:"注册成功",
+                            type:"success",
+                            timer:1000,
+                            showConfirmButton:false
+                        });
+                        /*window.setTimeout("location.href='sign_in.html'",1200);*/
+                        $state.go("sign_in.html");
+                    })
+                }else {
                     swal({
-                        title:"注册成功",
-                        type:"success",
+                        title:"两次输入密码不一致！",
+                        type:"error",
                         timer:1000,
                         showConfirmButton:false
                     });
-                    /*window.setTimeout("location.href='sign_in.html'",1200);*/
-                    $state.go("sign_in.html");
-                })
+                    user.password = "";
+                    user.surePwd = "";
+
+                }
+
             }else {
                 console.log(user.nickname)
                 swal("必填项不能为空！","error");

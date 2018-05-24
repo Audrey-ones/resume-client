42dd244c9df09c4efafae2413fd317147d110873
251918489@qq.coom <ludan123>
2018-05-24 17:48:36 +0800

    完善简历信息模块相关界面和个人中心界面

--- app/js/resumeInfoCtrl/basicInfoCtrl.js
+++ app/js/resumeInfoCtrl/basicInfoCtrl.js
@@ -7,13 +7,27 @@
             $('#myModal').modal('hide');
         }
 
-        $('#date1').datetimepicker({
+        basicInfoService.loadBasicInfo(function (data) {
+            console.log(data);
+            $scope.basicInfo = data.basicInfo;
+        })
+
+        $('#birthDate').datetimepicker({
             format: 'YYYY-MM-DD',
             locale: moment.locale('zh-cn')
         });
     }]);
 
     app.service("basicInfoService",["$http",function ($http) {
-
+        this.loadBasicInfo = function (callBack) {
+            $http({
+                url : "../../json/basicInfo.json",
+                method : "GET"
+            }).then(function (data) {
+                if (callBack){
+                    callBack(data.data);
+                }
+            })
+        }
     }])
 })();
